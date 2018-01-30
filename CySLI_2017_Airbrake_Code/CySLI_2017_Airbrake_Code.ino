60d76608060e64ca6dfef8b03039b7678287b000
Alex Harpenau <harpenau@iastate.edu>
2018-01-30 12:54:34 -0600

    moved GPS to EndGame and deleted the old BMP180 pressure stuff

--- CySLI_2017_Airbrake_Code/CySLI_2017_Airbrake_Code.ino
+++ CySLI_2017_Airbrake_Code/CySLI_2017_Airbrake_Code.ino
@@ -100,8 +100,6 @@ if(burnout){
 
 EndGame();
 
-GPSloop();
-
 WriteData();
 }
 //--------------------------------------------------------------Beginning of the Functions---------------------------------------------
@@ -270,56 +268,6 @@ void ms5611Setup(){
 
 double GetPressure(){
   return ms5611.readPressure();
-//  char status;
-//  double T,P,p0,a;
-//
-//  // You must first get a temperature measurement to perform a pressure reading.
-//  
-//  // Start a temperature measurement:
-//  // If request is successful, the number of ms to wait is returned.
-//  // If request is unsuccessful, 0 is returned.
-//
-//  status = ms5611.readTemperature();
-//  if (status != 0) {
-//    // Wait for the measurement to complete:
-//
-//    delay(status);
-//
-//    // Retrieve the completed temperature measurement:
-//    // Note that the measurement is stored in the variable T.
-//    // Use '&T' to provide the address of T to the function.
-//    // Function returns 1 if successful, 0 if failure.
-//
-//    status = ms5611.readTemperature(T);
-//    if (status != 0) {
-//      // Start a pressure measurement:
-//      // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
-//      // If request is successful, the number of ms to wait is returned.
-//      // If request is unsuccessful, 0 is returned.
-//
-//      status = ms5611.startPressure(3);  // is this worth it to go 3?
-//      if (status != 0) {
-//        // Wait for the measurement to complete:
-//        delay(status);
-//
-//        // Retrieve the completed pressure measurement:
-//        // Note that the measurement is stored in the variable P.
-//        // Use '&P' to provide the address of P.
-//        // Note also that the function requires the previous temperature measurement (T).
-//        // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
-//        // Function returns 1 if successful, 0 if failure.
-//
-//        status = ms5611.getPressure(P,T);
-//        if (status != 0) {
-//          return(P);
-//        }
-//        else Serial.println("error retrieving pressure measurement\n");
-//      }
-//      else Serial.println("error starting pressure measurement\n");
-//    }
-//    else Serial.println("error retrieving temperature measurement\n");
-//  }
-//  else Serial.println("error starting temperature measurement\n");
 }
 
 void GetAlt(){
@@ -499,6 +447,7 @@ void EndGame(){
     while(true){        //brakes closed, flight data will be logged until computer is turned off
       UpdateData();
       WriteData();
+      GPSloop();
     }
   } 
 }
